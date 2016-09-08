9e01bfa0290756512c554d6a35f696e8d7b9cc4f
Richard <rjk6qe@virginia.edu>
2016-09-08 17:56:30 -0400

    New User model created

--- fafs_api/models.py
+++ fafs_api/models.py
@@ -1,3 +1,41 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 # Create your models here.
+
+
+class School(models.Model):
+	name = models.CharField(max_length=75)
+	city = models.CharField(max_length=50)
+	state = models.CharField(max_length=30)
+
+class UserManager(BaseUserManager):
+
+	def create_user(self, email, school, password=None):
+		if not (email and school):
+			raise ValueError("Need an email!")
+		
+		u = self.model(
+			email=UserManager.normalize_email(email),
+			school_id = school,
+			)
+		u.set_password(password)
+		u.save()
+		return u
+
+	def create_superuser(self, email, password=None):
+		u = self.create_user(email, password)
+		u.is_superuser = True
+		u.save()
+		return u
+
+class User(AbstractBaseUser):
+	school_id = models.OneToOneField(School)
+	email = models.EmailField(unique=True, blank=False)
+	rating = models.IntegerField(blank=True, null=True)
+	phone_number = models.CharField(max_length=20, blank=True)
+	objects = UserManager()
+
+	USERNAME_FIELD = 'email'
+	
+
+
--- fafs_api/tests.py
+++ fafs_api/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.contrib.auth import authenticate
+from fafs_api.models import User, School, UserManager
 
 # Create your tests here.
+
+class test_api(TestCase):
+
+	set_password = 'helloworld'
+	set_email = 'rjk6qe@virginia.edu'
+
+	def setUp(self):
+		s = School.objects.create(name='UVA', city='Charlottesville', state='Virginia')
+		User.objects.create_user(email=self.set_email, school = s, password=self.set_password)
+
+	def test_create_school(self):
+		self.assertEqual(len(School.objects.all()),1, "School was not created")
+
+	def test_create_user(self):
+		self.assertNotEqual(User.objects.get().password, self.set_password, "Password was not hashed")
+
+	def test_authenticate_user(self):
+		user = authenticate(username=self.set_email, password= self.set_password)
+		self.assertNotEqual(user, None, "User could not be authenticated")
