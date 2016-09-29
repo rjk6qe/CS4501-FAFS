2fbaeb013571a45a218b92939f91bf76e1a9b7e2
vgupta <tg3dc@virginia.edu>
2016-09-28 20:42:04 -0400

    created templates

--- fafs_web/fafs_api/views.py
+++ fafs_web/fafs_api/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
-
-# Create your views here.
-
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    #return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'fafs_api/index.html')
