2e143897cafb4b5fd9257428722183360486c04b
vgupta <tg3dc@virginia.edu>
2016-10-04 11:12:53 -0400

    working URLS

--- fafs_exp/fafs_api/views.py
+++ fafs_exp/fafs_api/views.py
@@ -12,6 +12,4 @@ def index(request):
     req = urllib.request.Request('http://models-api:8000/api/categories/')
     resp_json = urllib.request.urlopen(req).read().decode('utf-8')
     #resp = json.loads(resp_json)
-    print(resp_json)
-    #return resp
     return HttpResponse(resp_json)
