5bbaa4352cc2b69536b5055a1abfbe52e0f4ee43
Richard <rjk6qe@virginia.edu>
2016-10-05 18:48:07 -0400

    cleaned up exp views, made everything jsonresponses

--- fafs_exp/fafs_api/views.py
+++ fafs_exp/fafs_api/views.py
@@ -1,46 +1,57 @@
 from django.shortcuts import render
 import urllib.request
 import urllib.parse
+#import requests
 import json
 from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 API_URL = 'http://models-api:8000/api/'
 
+
+def append_to_url(path_list):
+    url = API_URL
+    if path_list is not None:
+        for path in path_list:
+            url = url + str(path) + '/'
+    return url    
+
 def make_request(path):
     req = urllib.request.Request(API_URL + path)
     resp_json = urllib.request.urlopen(req).read().decode('utf-8')
     return json.loads(resp_json)
 
+def get_request(path_list=None):
+    url = append_to_url(path_list)
+    #response = requests.get(url)
+    req = urllib.request.Request(url)
+    json_response = urllib.request.urlopen(req).read().decode('utf-8')
+    return json.loads(json_response)
+
+def post_request(path_list, data):
+    url = append_to_url(path_list)
+    return response.json()
+
 def json_encode_dict_and_status(dictionary, status):
-	response_dict = {}
-	response_dict["status"] = status
-	response_dict["response"] = dictionary
-	return response_dict
+    response_dict = {}
+    response_dict["status"] = status
+    response_dict["response"] = dictionary
+    return response_dict
 
 def getCategories(request):
-
-    req = urllib.request.Request('http://models-api:8000/api/categories/')
-    resp_json = urllib.request.urlopen(req).read().decode('utf-8')
-    #resp = json.loads(resp_json)
-    return HttpResponse(resp_json)
+    response = get_request(['categories',])
+    return JsonResponse(response)
 
 def getProducts(request):
-
-    req = urllib.request.Request('http://models-api:8000/api/products/')
-    resp_json = urllib.request.urlopen(req).read().decode('utf-8')
-    #resp = json.loads(resp_json)
-    return HttpResponse(resp_json)
+    response = get_request(['products',])
+    return JsonResponse(response)
 
 def getProduct(request, pk):
-	product_data = make_request('products/' + pk)
-	return JsonResponse(json_encode_dict_and_status(product_data['response'], True))
+    response = get_request(['products',pk])
+    return JsonResponse(response)
 
 def getLatestProducts(request, num=3):
-    req = urllib.request.Request(API_URL + 'products/')
-    resp_json = urllib.request.urlopen(req).read().decode('utf-8')
-    resp = json.loads(resp_json)
-    product_data = resp['response']
+    response = get_request(['products',])
+    product_data = response['response']
     sorted_products = sorted(product_data, key = lambda x: x["time_posted"], reverse=True)[:int(num)]
-
     return JsonResponse(json_encode_dict_and_status(sorted_products, True))
