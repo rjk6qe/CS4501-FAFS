0355ce398a2497ea7a764507075effdd112bf5fd
vgupta <tg3dc@virginia.edu>
2016-10-23 15:38:42 -0400

    fixed template for registration

--- fafs_models/fafs_api/views.py
+++ fafs_models/fafs_api/views.py
@@ -169,7 +169,8 @@ class UserView(View):
 					"pk":queryset.pk,
 					"email":queryset.email,
 					"school_id":queryset.school_id.pk,
-					"rating":queryset.rating
+					"rating":queryset.rating,
+					"password":queryset.password
 					}
 			else:
 				status = False
@@ -177,7 +178,7 @@ class UserView(View):
 		else:
 			status = True
 			queryset = self.model.objects.all()
-			json_data = list(queryset.values('pk','email','school_id','rating'))
+			json_data = list(queryset.values('pk','email','school_id','rating', 'password'))
 
 		return JsonResponse(json_encode_dict_and_status(json_data, status))
 
