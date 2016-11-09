87152d1083432777a9b7f86394a3be1b98e31a67
Brian Nguyen <btn3gj@virginia.edu>
2016-11-09 11:50:10 -0500

    Fixed kafka not ready error

--- batch/kafka_to_es.py
+++ batch/kafka_to_es.py
@@ -1,7 +1,17 @@
 from kafka import KafkaConsumer
+from kafka.common import NodeNotReadyError
 from elasticsearch import Elasticsearch
+import time
 
-consumer = KafkaConsumer('new-listings-topic', group_id='listing-indexer', bootstrap_servers=['kafka:9092'])
+kafka_ready = False
+
+while not kafka_ready:
+    try:
+        consumer = KafkaConsumer('new-listings-topic', group_id='listing-indexer', bootstrap_servers=['kafka:9092'])
+        kafka_ready = True
+    except NodeNotReadyError:
+        time.sleep(5)
+
 for message in consumer:
     kafka_object = json.loads((message.value).decode('utf-8'))
     index_status = es.index(index='listing_index', doc_type='listing', id=kakfa_object['pk'], body=kakfa_object)
