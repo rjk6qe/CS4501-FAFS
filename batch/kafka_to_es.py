728871e83dd8329b6d2b6bf6b026d7ec103378a7
Brian Nguyen <btn3gj@virginia.edu>
2016-11-09 11:37:54 -0500

    Added running script

---
+++ batch/kafka_to_es.py
@@ -0,0 +1,8 @@
+from kafka import KafkaConsumer
+from elasticsearch import Elasticsearch
+
+consumer = KafkaConsumer('new-listings-topic', group_id='listing-indexer', bootstrap_servers=['kafka:9092'])
+for message in consumer:
+    kafka_object = json.loads((message.value).decode('utf-8'))
+    index_status = es.index(index='listing_index', doc_type='listing', id=kakfa_object['pk'], body=kakfa_object)
+    es.indices.refresh(index="listing_index")
