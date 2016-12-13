7eeb20caf3ab55de654e26df172f3218005af286
vgupta <tg3dc@virginia.edu>
2016-12-13 16:27:01 -0500

    added print statements

--- data/coviews.py
+++ data/coviews.py
@@ -19,8 +19,18 @@ data = data.distinct() # Make distinct rows
 pairs = data.map(lambda line: line.split("\t"))   # tell each worker to split each line of it's partition
 
 pages = pairs.map(lambda pair: (pair[0], pair[1]))   # Map to (user_id, item_id) (#1)
+output = pages.collect()
+print ("(#1) Map to (user_id, item_id) ")
+for key,value in output:
+    print("Pages: user ID %s and item_id %s" % (key, value))
+print ("done printing pages")
 
 users_with_list = pages.groupByKey() # Group item_ids to user_id: (user_id, list of item ids they clicked on) (#2)
+output = users_with_list.mapValues(list).collect()
+print ("(#2)Group item_ids to user_id: (user_id, list of item ids they clicked on)")
+for key,value in output:
+    print("Pages: user ID %s and items clicked %s" % (key, value))
+print ("done printing users_with_list")
 
 def return_pairs(user_id, list_of_ids):
     resulting_list = []
@@ -34,20 +44,35 @@ def return_pairs(user_id, list_of_ids):
 
 # (#3) Transform into (user_id, (item1, item2) where item1 and item2 are pairs of items the user clicked on
 users_with_pairs = users_with_list.flatMap(lambda data: return_pairs(data[0], data[1]))
+output = users_with_pairs.collect()
+print ("(#3) Transform into (user_id, (item1, item2) where item1 and item2 are pairs of items the user clicked on")
+for key,value in output:
+    print("Pages: user ID %s and pair %s" % (key, value))
+print ("done printing users_with_pairs")
 
 # Switch pairs
 pairs_with_users = users_with_pairs.map(lambda pair: (pair[1], pair[0]))
 
 # (#4) Transform into ((item1, item2), list of user1, user2 etc) where users are all the ones who co-clicked (item1, item2)
 pairs_with_users = pairs_with_users.groupByKey()
+output = pairs_with_users.mapValues(list).collect()
+print ("(#4) Transform into ((item1, item2), list of user1, user2 etc) where users are all the ones who co-clicked (item1, item2)")
+for key,value in output:
+    print("Pages: pairs of items clicked ID %s and users list %s" % (key, value))
+print ("done printing pairs_with_users")
 
 # (#5) Transform into ((item1, item2), count of distinct users who co-clicked (item1, item2)
 pairs_with_users_count = pairs_with_users.map(lambda pair: (pair[0], len(pair[1])))
+output = pairs_with_users_count.collect()
+print ("(#5) Transform into ((item1, item2), count of distinct users who co-clicked (item1, item2)")
+for key,value in output:
+    print("Pages: pair %s and count %s" % (key, value))
+print ("done printing pairs_with_users_count")
 
 # (#6) Filter out any results where less than 3 users co-clicked the same pair of items
 filtered_pairs_with_users_count = pairs_with_users_count.filter(lambda pair: pair[1] >= 3)
-
 output = filtered_pairs_with_users_count.collect()
+print ("(#6) Filter out any results where less than 3 users co-clicked the same pair of items")
 for key,value in output:
     print("Co-clicks: %s and %s with count: %d" % (key[0], key[1], value))
 print("Co-clicks done")
